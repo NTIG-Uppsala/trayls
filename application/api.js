0357887636371a83c08be7170022e7c96bbdf397
ViktorIsenborg <89513274+ViktorIsenborg@users.noreply.github.com>
2021-11-18 10:53:56 +0100

    Application errors
    localhost does not work because of CORS policy and various security reasons.

--- application/api.js
+++ application/api.js
@@ -1,12 +1,14 @@
 function UserAction() {
-    console.log("hej");
+    console.log("___________");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
          if (this.readyState == 4 && this.status == 200) {
              alert(this.responseText);
          }
     };
-    xhttp.open("GET", "localhost:8080/getTrip/uppsala/Ekolns%20segelkubb", true);
+    xhttp.open("open", "http://localhost:8080/getTrip/uppsala/Ekolns%20segelkubb", true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
+    /*line 12: "Failed to load resource: net::ERR_FAILED"*/
+    xhttp.send('');
+    let data = {'uppsala':'svandammen'}
 }
