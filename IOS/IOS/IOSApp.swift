2520fa423c04f802c8a7ebb1a437f3f1cdf01643
Albin Eriksson <albin.eriksson3@elev.ga.ntig.se>
2021-11-22 09:44:10 +0100

    added IOS files

---
+++ IOS/IOS/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  IOS
+//
+//  Created by te4 on 2021-11-22.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
