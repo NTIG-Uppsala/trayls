f549903316e24ec39b37765033d8ff07b0a1d27d
ViktorIsenborg <89513274+ViktorIsenborg@users.noreply.github.com>
2021-11-23 10:17:07 +0100

    main.py reads task.json

--- python/main.py
+++ python/main.py
@@ -3,9 +3,11 @@ import flask
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+f = open("python/task.json", "r")
 
-@app.route('/getTask', methods=['GET'])
+@app.route("/getTask", methods=["GET"])
 def home():
-    return "hej"
+    return f.read()
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host = "0.0.0.0", port = 8080)
+
