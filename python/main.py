1122759558cc2140afc166b2353ff00d50356fc2
ViktorIsenborg <89513274+ViktorIsenborg@users.noreply.github.com>
2021-11-23 15:15:11 +0100

    Flask API specific outputs with ASCII and UTF-8

--- python/main.py
+++ python/main.py
@@ -1,13 +1,18 @@
 import flask
+import json
+
+jsonfile = json.load(open('task.json', encoding="utf-8"))
+
+def randomizeOutput():
+    return jsonfile["task"]["motivation"][1]
 
 app = flask.Flask(__name__)
+app.config["DECODE"] = "utf-8"
 app.config["DEBUG"] = True
 
-f = open("python/task.json", "r")
-
 @app.route("/getTask", methods=["GET"])
+
 def home():
-    return f.read()
+    return json.dumps(randomizeOutput(), ensure_ascii=False)
 
 app.run(host = "0.0.0.0", port = 8080)
-
