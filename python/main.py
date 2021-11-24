1a886bf534885114d91dcc9ef246d8a906be6fc3
ViktorIsenborg <89513274+ViktorIsenborg@users.noreply.github.com>
2021-11-24 09:57:13 +0100

    Changed output and added comments
    Removed curly brackets and colons from the output

--- python/main.py
+++ python/main.py
@@ -1,20 +1,27 @@
 import flask
 import json
-from random import randrange
+import random
 
+# load the json file from directory and opens it
 jsonfile = json.load(open('task.json', encoding="utf-8"))
 
+# locates a specific array and randomly picks one
 def randomizeOutput():
-    x = randrange(0, 3)
-    return jsonfile["task"]["motivation"][x]
+    x = random.choice(jsonfile ["task"]["motivation"])
+    return x["daily"]
 
+# configuration for non-english characters, debugging and interprets flask module
 app = flask.Flask(__name__)
 app.config["DECODE"] = "utf-8"
 app.config["DEBUG"] = True
 
+# URL of the output
 @app.route("/getTask", methods=["GET"])
 
+# function that dumps the output of the randomly picked array
 def home():
+    
     return json.dumps(randomizeOutput(), ensure_ascii=False)
 
+# runs localhost
 app.run(host = "0.0.0.0", port = 8080)
