52c1bc482eda98d68c0851996d4c693cac1268eb
Christoffer Ljung Ahlstedt <christoffer.ljungahlstedt@gmail.com>
2021-11-25 10:02:40 +0100

    The node js API

---
+++ node/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { json } = require('express');
+//basic api express
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//Middleware to parse json
+app.use(express.json());
+
+//Start upp api to listen
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+//Simple GET request
+app.get('/getTask', (req, res) => {
+    res.send(randomTask())
+});
+
+//read from json
+function readFromJson() {
+    const fs = require('fs');
+    let rawdata = fs.readFileSync('task.json');
+    return JSON.parse(rawdata);
+}
+
+function randomTask() {
+    const tasks = readFromJson();
+    const arrayPos = Math.floor(Math.random() * tasks.task.motivation.length);
+    let hej = tasks.task.motivation[arrayPos].daily;
+    return hej;
+}
+
+//Middleware takes care of 404
+//If higher up in code, it will be called first for some reason. And wont go through with any api calls
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found. ERROR: 404'}) //send back 404
+});
