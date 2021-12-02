a4ef1fe3179c63617950d655b2e24aa352f92eb6
Christoffer Ljung Ahlstedt <christoffer.ljungahlstedt@gmail.com>
2021-12-02 14:22:23 +0100

    moved code from sever to git folder

--- sqlnode/app.js
+++ sqlnode/app.js
@@ -1,33 +1,105 @@
+'use strict';
+
+//Basic API express
+const express = require('express');
 const mariadb = require('mariadb');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
 const pool = mariadb.createPool({
     host: 'localhost',
-	user: 'task_user',
-	password: 'Hej123hej',
-	database: 'trayls'
+	user: 'admin',
+	password: 'admin123',
+	database: 'trayls',
+	connectionLimit : 20
 });
-async function getTaskFromDb(table, task_id) {
+
+//Middleware to parse json
+app.use(express.json());
+
+//Starts the API for it to listen
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+//Simple GET request
+app.get('/getTask', async function(req, res) {
+	const senddata = await getDataFromDb('task_query','traylsdb','task_id',getRandomInt(19));
+    res.send(senddata);
+});
+
+app.get('/getApiTest', (req, res) => {
+    res.send('API works');
+});
+
+app.post('/addUser', async function(req, res) {
+    const { mail } = req.body;
+	console.log(mail)
+    const userId = await getDataFromDb('user_id','users','user_mail',`'${mail}'`);
+	console.log(userId);
+	if (userId == 'Not found') {
+		await addToDb(`'${mail}'`);
+		console.log('User does not exist, added it to database');
+		res.send(`Welcome new user :) ${mail}`);
+	} else {
+		console.log('User exist');
+		res.send(`Welcome ${mail}`);
+	}
+	
+});
+
+async function addToDb(mail) {
 	let conn;
 	try {
 		conn = await pool.getConnection();
-		const rows = await conn.query(`SELECT * FROM ${table} traylsdb WHERE task_id = ${task_id}`);
-		removeMeta(rows);
-        console.log(removeMeta(rows));
-
+		//insert user in db
+		conn.query(`INSERT INTO users(user_id, user_mail) VALUES (NULL, ${mail})`);
 	} catch (err) {
-		console.log('ERROR!!!')
+		console.log('Failed');
 		throw err;
 	} finally {
 		if (conn) {
-			return conn.end();
+			conn.end();
+			return response;
 		}
 	}
 }
 
-function removeMeta(parsedJson) {
-    delete parsedJson['meta'];
-	return parsedJson;
+
+
+async function getDataFromDb(data, table, colVal, id) {
+	let conn;
+	let APIresponse;
+	try {
+		conn = await pool.getConnection();
+		const rows = await conn.query(`SELECT ${data} FROM ${table} WHERE ${colVal} = ${id}`);
+		removeMeta(rows);
+        console.log(removeMeta(rows, data));
+		APIresponse = removeMeta(rows) 
+				
+	} catch (err) {
+		console.log('ERROR!!!')
+		APIresponse = 'Not found';
+		throw err;
+	} finally {
+		if (conn) {
+			conn.end();
+			return APIresponse
+		}
+	}
 }
 
-let value = 10;
-getTaskFromDb('traylsdb', value);
-getTaskFromDb('taskvalues', value);
+function removeMeta(parsedJson, data) {
+    delete parsedJson['meta'];
+	return parsedJson[0][data];
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+//Middleware takes care of 404
+//If higher up in code, it will be called first for some reason, and will not go through with any API calls
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found. ERROR: 404'}) //send back 404
+});
