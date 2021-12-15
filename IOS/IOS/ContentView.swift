1cb24677d62cbfffbaffab54a07132bf6fa0df1e
Albin Eriksson <albin.eriksson3@elev.ga.ntig.se>
2021-12-15 16:01:34 +0100

    new ios app merge conflict studio

--- IOS/IOS/ContentView.swift
+++ IOS/IOS/ContentView.swift
@@ -9,26 +9,20 @@
 import SwiftUI
  
 struct ContentView: View {
- 
-    @State private var contents = " "
-    
     var body: some View {
-
+        
         VStack() {
             
             Text("Trayls")
                 .frame(width: 400, height: 100)
                 .foregroundColor(.black)
                 .background(Color(red: 43 / 255, green: 174 / 255, blue: 102 / 255))
-                .cornerRadius(15)
-                .shadow(radius: 20)
                 .font(.custom("Times New Roman", size:50))
             
             NavigationView{
-            NavigationLink(destination: SecondContentView() .navigationBarBackButtonHidden(true)){
+            NavigationLink(destination: SecondContentView() .navigationBarHidden(true)){
                 Text("Ny task")
                    
-
                     .frame(width: 180, height: 40)
                     .foregroundColor(.black)
                     .padding()
@@ -38,6 +32,7 @@ struct ContentView: View {
                     .font(.custom("Times New Roman", size:30))
             }
 
+
             }
 
             Spacer()
@@ -45,39 +40,14 @@ struct ContentView: View {
                 .frame(width: 400, height: 80)
                 .foregroundColor(.black)
                 .background(Color(red: 43 / 255, green: 174 / 255, blue: 102 / 255))
-                .cornerRadius(15)
-                .shadow(radius: 20)
                 .font(.custom("Times New Roman", size:30))
-            
-        
-                
-         // or .top
+
         
         }
         .blendMode(.darken)
         .edgesIgnoringSafeArea(.vertical)
     } // End body
      
-
-    func GetTask(){
-        if let url = URL(string: "netlabua.se/task") {
-        do {
-           contents = try String(contentsOf: url)
-            
-        } catch {
-            
-        }
-    } else {
-        print("Invalid URL")
-    }
-
-        struct Task{
-            var contents:String
-        }
- 
-        
-
-
  
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -87,8 +57,158 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct SecondContentView: View{
+    
+    @State private var contents = " "
+    @State private var parsedTask = " "
+    @State private var parsedPoints = " "
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
         var body: some View{
-            Text()
+            
+            Text(parsedTask)
+                .frame(width: 280, height: 200)
+                .foregroundColor(.black)
+                .padding()
+                .background(Color(red: 43 / 255, green: 174 / 255, blue: 102 / 255))
+                .cornerRadius(15)
+                .shadow(radius: 20)
+                .font(.custom("Times New Roman", size:30))
+            
+                NavigationView{
+                    VStack {
+                    NavigationLink(destination: ThirdContentView() .navigationBarHidden(true)){
+                        Text("Acceptera")
+                            .frame(width: 180, height: 40)
+                            .foregroundColor(.black)
+                            .padding()
+                            .background(Color(red: 43 / 255, green: 174 / 255, blue: 102 / 255))
+                            .cornerRadius(15)
+                            .shadow(radius: 20)
+                            .font(.custom("Times New Roman", size:30))
+                    }
+                        Button(action: { self.presentationMode.wrappedValue.dismiss()
+                                }, label: {
+                                    Text("Neka")
+                                        .frame(width: 180, height: 40)
+                                        .foregroundColor(.black)
+                                        .padding()
+                                        .background(Color(red: 255 / 255, green: 10 / 255, blue: 10 / 255))
+                                        .cornerRadius(15)
+                                        .shadow(radius: 20)
+                                        .font(.custom("Times New Roman", size:30))
+                            }
+                        )
+                    }
         }
+                .blendMode(.darken)
+        
+            .onAppear {
+                self.GetTask()
+            
+            }
+        
+        }
+    
+    func GetTask(){
+        if let url = URL(string: "http://netlabua.se/task") {
+        do {
+           contents = try String(contentsOf: url)
+            let jsonData = contents.data(using: .utf8)!
+            // Convert the JSON object to a Video object
+            let parsed: Source = try! JSONDecoder().decode(Source.self, from: jsonData)
+
+            let parsedtask = parsed.task_query
+            let parsedPoints = parsed.task_points// Output: JSON Made Simple
+            let parsedTaskId = parsed.task_id
+            print(parsedTaskId)
+            print(parsedPoints)
+            print(parsedtask)
+            
+        } catch {
+            
+        }
+    } else {
+        print("Invalid URL")
     }
+        
+        
+
+        struct Task{
+            var contents:NSObject
+        }
+    
+    struct Source: Decodable{
+        
+        var task_id: Int
+        var task_query: String
+        var task_points: Int
+    
+    }
+
+
+
 }
+
+struct ThirdContentView: View{
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State private var contents = " "
+    @State private var parsed = " "
+    
+        var body: some View{
+        
+                NavigationView{
+                    HStack {
+                    NavigationLink(destination: EmptyView() .navigationBarBackButtonHidden(true)){
+                        Text("Klar")
+                            .frame(width: 100, height: 40)
+                            .foregroundColor(.black)
+                            .padding()
+                            .background(Color(red: 43 / 255, green: 174 / 255, blue: 102 / 255))
+                            .cornerRadius(15)
+                            .shadow(radius: 20)
+                            .font(.custom("Times New Roman", size:30))
+                    }
+
+                        
+                            Button(action: { self.presentationMode.wrappedValue.dismiss()
+                                    }, label: {
+                                        Text("Avbryt")
+                                            .frame(width: 100, height: 40)
+                                            .foregroundColor(.black)
+                                            .padding()
+                                            .background(Color(red: 255 / 255, green: 10 / 255, blue: 10 / 255))
+                                            .cornerRadius(15)
+                                            .shadow(radius: 20)
+                                            .font(.custom("Times New Roman", size:30))
+                                })
+                        }
+                               
+                               
+                               
+                }
+                .blendMode(.darken)
+            
+            }
+        
+        
+
+struct EmptyContentView: View{
+    
+    @State private var contents = " "
+    
+        var body: some View{
+        
+                Text("")
+            
+            }
+        
+        }
+    
+        
+    
+
+
+}
+}
+
+
