abe8b9c067d4f81cf8a840b8b856e0fa0d29b1c1
Albin Eriksson <albin.eriksson3@elev.ga.ntig.se>
2021-12-15 09:47:22 +0100

    added design for app IOS

--- IOS/IOS/ContentView.swift
+++ IOS/IOS/ContentView.swift
@@ -13,32 +13,54 @@ struct ContentView: View {
     @State private var contents = " "
     
     var body: some View {
-         
-        VStack {
-            Button(action: {GetTask()
-            }, label: {
-                Text("Ny Uppgift")
-                    .fontWeight(.heavy)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(15)
-            })
-                .onAppear(perform: {GetTask()})
-            Text("Nästa Aktivitet är:\n\(self.contents)")
+
+        VStack() {
             
-                .fontWeight(.heavy)
-                .foregroundColor(.blue)
-                .padding(50)
+            Text("Trayls")
+                .frame(width: 400, height: 100)
+                .foregroundColor(.black)
+                .background(Color(red: 43 / 255, green: 174 / 255, blue: 102 / 255))
                 .cornerRadius(15)
+                .shadow(radius: 20)
+                .font(.custom("Times New Roman", size:50))
+            
+            NavigationView{
+            NavigationLink(destination: SecondContentView() .navigationBarBackButtonHidden(true)){
+                Text("Ny task")
+                   
+
+                    .frame(width: 180, height: 40)
+                    .foregroundColor(.black)
+                    .padding()
+                    .background(Color(red: 43 / 255, green: 174 / 255, blue: 102 / 255))
+                    .cornerRadius(15)
+                    .shadow(radius: 20)
+                    .font(.custom("Times New Roman", size:30))
+            }
+
+            }
+
+            Spacer()
+            Text(verbatim:"Inloggad som:\ntest@mail.com")
+                .frame(width: 400, height: 80)
+                .foregroundColor(.black)
+                .background(Color(red: 43 / 255, green: 174 / 255, blue: 102 / 255))
+                .cornerRadius(15)
+                .shadow(radius: 20)
+                .font(.custom("Times New Roman", size:30))
+            
         
                 
+         // or .top
+        
         }
+        .blendMode(.darken)
+        .edgesIgnoringSafeArea(.vertical)
     } // End body
      
 
     func GetTask(){
-        if let url = URL(string: "https://walk-api.azurewebsites.net/gettask") {
+        if let url = URL(string: "netlabua.se/task") {
         do {
            contents = try String(contentsOf: url)
             
@@ -48,14 +70,13 @@ struct ContentView: View {
     } else {
         print("Invalid URL")
     }
-        
-    
 
-     
         struct Task{
-            var contents:NSObject
+            var contents:String
         }
-     
+ 
+        
+
 
  
 struct ContentView_Previews: PreviewProvider {
@@ -64,4 +85,10 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 }
+
+struct SecondContentView: View{
+        var body: some View{
+            Text()
+        }
+    }
 }
