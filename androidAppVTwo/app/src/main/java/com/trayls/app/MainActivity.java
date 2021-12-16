90004584bda3347d70b753c1c7b32376e932cca9
JoelNymark <89513327+JoelNymark@users.noreply.github.com>
2021-12-16 15:22:19 +0100

    Androidapp Woorking with get task

--- androidAppVTwo/app/src/main/java/com/trayls/app/MainActivity.java
+++ androidAppVTwo/app/src/main/java/com/trayls/app/MainActivity.java
@@ -2,8 +2,9 @@ package com.trayls.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,10 +17,13 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.*;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView welcomeText;
+
+
 
     @Override
 
@@ -27,27 +31,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-/*
         Button getTaskBtn = (Button) findViewById(R.id.btnGetTask);
-        welcomeText = (TextView) findViewById(R.id.welcome);
+
+
+        welcomeText = (TextView) findViewById(R.id.textGetTask);
+
+
+
         getTaskBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 apiCallAndChangeText();
+                vibrateOnClick();
+                //getSupportFragmentManager().beginTransaction().replace(R.id.loginFragment, new AccRejFragment()).commit();
             }
-        });*/
+        });
     }
+
+
 
 
     private void apiCallAndChangeText(){
         RequestQueue queue = Volley.newRequestQueue(this);
 
         String url = "http://netlabua.se/task";
+        final String[] newResponse = new String[1];
 
         StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                welcomeText.setText(response.toString());
+                try {
+                    newResponse[0] = parseJson(response);
+                }catch (Exception e){
+                    newResponse[0] = "error was found";
+                }
+                welcomeText.setText(newResponse[0]);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -56,5 +73,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         queue.add(request);
+    }
+
+
+    public String parseJson(String response) throws JSONException {
+        JSONObject obj = new JSONObject(response);
+        return obj.getString("task_query");
+    }
+
+    private void vibrateOnClick() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(75 );
     }
 }
