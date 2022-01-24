4449a83a429a5f49cbdb9182fc1fd5cf68863e37
Sebastian Clavensjo <Sebastian.clavensjo@elev.ga.ntig.se>
2022-01-24 11:27:14 +0100

    added new api class

---
+++ frontend/trayls/lib/api.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Api {
+  Api ();
+  getTask() async {
+    var response = await http.get(Uri.http('netlabua.se', '/task'));
+    var jsonData = jsonDecode(response.body);
+
+    List<String> task = [];
+    task.add(jsonData['task_query']); 
+    task.add(jsonData['task_id'].toString()); 
+    task.add(jsonData['task_points'].toString()); 
+    return task;
+  }
+}
