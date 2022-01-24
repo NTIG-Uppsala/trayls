579d9134ead15f3483ac235b8f9b41584aabf3c3
Sebastian Clavensjo <Sebastian.clavensjo@elev.ga.ntig.se>
2022-01-24 13:05:13 +0100

    API call first draft

--- frontend/trayls/lib/api.dart
+++ frontend/trayls/lib/api.dart
@@ -2,7 +2,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Api ();
+  List<String> taskResponse = [];
+  Api (){
+    taskResponse = getTask();
+  }
   getTask() async {
     var response = await http.get(Uri.http('netlabua.se', '/task'));
     var jsonData = jsonDecode(response.body);
--- frontend/trayls/lib/taskpage.dart
+++ frontend/trayls/lib/taskpage.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:trayls/api.dart';
 
 class TaskPage extends StatefulWidget {
   const TaskPage({Key? key}) : super(key: key);
@@ -10,7 +11,7 @@ class TaskPage extends StatefulWidget {
 }
 
 class _TaskPageState extends State<TaskPage> {
-  getTask() async {
+   getTask() async {
     var response = await http.get(Uri.http('netlabua.se', '/task'));
     var jsonData = jsonDecode(response.body);
 
@@ -20,9 +21,12 @@ class _TaskPageState extends State<TaskPage> {
     task.add(jsonData['task_points'].toString()); 
     return task;
   }
+ 
+  //Api api = Api();
 
   @override
   Widget build(BuildContext context) {
+    //print(api);
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 100,
