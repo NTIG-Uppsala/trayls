cd9bd5d43d8709f0025d8b66f1063416c1fb54bd
Christoffer Ljung Ahlstedt <christoffer.ljungahlstedt@gmail.com>
2022-01-25 14:53:04 +0100

    Parent class for all the api calls added

--- frontend/trayls/lib/api.dart
+++ frontend/trayls/lib/api.dart
@@ -1,18 +1,14 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
-class Api {
-  List<String> taskResponse = [];
-  Api() {
-    taskResponse = getTask();
-  }
-  getTask() async {
-    var response = await http.get(Uri.http('netlabua.se', '/task'));
-    var jsonData = jsonDecode(response.body);
 
-    List<String> task = [];
-    task.add(jsonData['task_query']);
-    task.add(jsonData['task_id'].toString());
-    task.add(jsonData['task_points'].toString());
-    return task;
+class Api {
+  final String subdirectory;
+  final String secondLevelDomain = 'netlabua.se/';
+
+  Api({required this.subdirectory});
+
+  //connect to the server and get the data
+  Future<dynamic> get() async {
+    var response = await http.get(Uri.http(secondLevelDomain, subdirectory));
+    return response;
   }
 }
