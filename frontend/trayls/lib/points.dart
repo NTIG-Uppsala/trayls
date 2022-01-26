2df5f31af6db9ef3432f287b1cfd32fc59aa30d3
Christoffer Ljung Ahlstedt <christoffer.ljungahlstedt@gmail.com>
2022-01-26 15:17:47 +0100

    Points can now be displayed

--- frontend/trayls/lib/points.dart
+++ frontend/trayls/lib/points.dart
@@ -1,23 +1,30 @@
-import 'dart:async';
 import 'dart:convert';
+import 'dart:async';
+import 'dart:developer';
 import 'package:trayls/api.dart';
 
 class Points {
-  final String mail;
-  final int points;
+  String mail = '';
+  int points = 0;
+  Map<String, dynamic> query = {};
 
-  Points({required this.mail, required this.points});
+  Points({required this.mail}) {
+    query['mail'] = mail;
+  }
+
+  Points.res({required this.points});
 
   factory Points.fromJson(Map<String, dynamic> json) {
-    return Points(mail: json['user_mail'], points: json['user_points']);
+    print('hej');
+    return Points.res(points: json['user_points']);
   }
 }
 
-Future<Points> getPoints() async {
-  Api api = Api(subdirectory: '/points');
+Future<Points> getPoints(Map<String, dynamic> query) async {
+  Api api = Api.query(subdirectory: '/points', queryParams: query);
   var response = await api.get();
   if (response.statusCode == 200) {
-    return Points.fromJson(response.body);
+    return Points.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to load points');
   }
