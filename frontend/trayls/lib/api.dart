8da9353b54ea5e92c1d2c7a3c586698e23614024
Christoffer Ljung Ahlstedt <christoffer.ljungahlstedt@gmail.com>
2022-01-26 13:23:56 +0100

    Big problem with api calls, get req cant have body

--- frontend/trayls/lib/api.dart
+++ frontend/trayls/lib/api.dart
@@ -1,14 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class Api {
   final String subdirectory;
   final String secondLevelDomain = 'netlabua.se';
+  String callBody = '';
 
   Api({required this.subdirectory});
+  Api.body({required this.subdirectory, required this.callBody});
 
-  //connect to the server and get the data
+  //connect to the server and get the data with body
   Future<dynamic> get() async {
-    var response = await http.get(Uri.http(secondLevelDomain, subdirectory));
+    var response = await http.get(Uri.http(secondLevelDomain, subdirectory),
+        body: jsonEncode(<String, String>{'body': callBody}));
     return response;
   }
+
+  final http.Request request =
+      http.Request('GET', Uri.http('netlabua.se', subdirectory));
 }
--- frontend/trayls/lib/points.dart
+++ frontend/trayls/lib/points.dart
@@ -12,3 +12,13 @@ class Points {
     return Points(mail: json['user_mail'], points: json['user_points']);
   }
 }
+
+Future<Points> getPoints() async {
+  Api api = Api(subdirectory: '/points');
+  var response = await api.get();
+  if (response.statusCode == 200) {
+    return Points.fromJson(response.body);
+  } else {
+    throw Exception('Failed to load points');
+  }
+}
